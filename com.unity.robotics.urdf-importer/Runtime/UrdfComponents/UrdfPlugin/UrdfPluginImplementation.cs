f3b10173c94fcef64d9224785104b4c7cd2b8dc1
Alec Tutin <a.tutin@qut.edu.au>
2021-11-22 17:03:03 +1000

    Adding more methods to access parameters in the URDFs.

--- com.unity.robotics.urdf-importer/Runtime/UrdfComponents/UrdfPlugin/UrdfPluginImplementation.cs
+++ com.unity.robotics.urdf-importer/Runtime/UrdfComponents/UrdfPlugin/UrdfPluginImplementation.cs
@@ -56,6 +56,22 @@ namespace Unity.Robotics.UrdfImporter
             }
             return true;
         }
+
+        public static bool GetXAttribute(XElement node, string attributeName, out XAttribute result, bool required = true)
+        {
+            result = node.Attribute(attributeName);
+            if (result == null)
+            {
+                if (required)
+                {
+                    throw new Exception($"Node with name {node.Name} missing attribute: {attributeName}");
+                }
+
+                return false;
+            }
+
+            return true;
+        }
         
         public static bool ReadStringFromXElement(XElement node, string childElementName, out string result, 
             bool required = true, string defaultValue = "")
@@ -67,6 +83,19 @@ namespace Unity.Robotics.UrdfImporter
             }
             
             result = childXElement.Value;
+            return true;
+        }
+
+        public static bool ReadStringFromXElementAttribute(XElement node, string attributeName, out string result,
+            bool required = true, string defaultValue = "")
+        {
+            if (!GetXAttribute(node, attributeName, out XAttribute xAttribute, required))
+            {
+                result = defaultValue;
+                return false;
+            }
+
+            result = xAttribute.Value;
             return true;
         }
         
@@ -82,12 +111,7 @@ namespace Unity.Robotics.UrdfImporter
             bool parseSuccess = int.TryParse(childXElement.Value, out result);
             if (!parseSuccess)
             {
-                if (required)
-                {
-                    throw new Exception($"Node {node.Name} value expected an integer, received: {childXElement.Value}");
-                }
-                result = defaultValue;
-                return false;
+                throw new Exception($"Node {node.Name} value expected an integer, received: {childXElement.Value}");
             }
             return true;
         }
@@ -104,12 +128,24 @@ namespace Unity.Robotics.UrdfImporter
             bool parseSuccess = double.TryParse(childXElement.Value, out result);
             if (!parseSuccess)
             {
-                if (required)
-                {
-                    throw new Exception($"Node {node.Name} value expected a double, received: {childXElement.Value}");
-                }
+                throw new Exception($"Node {node.Name} value expected a double, received: {childXElement.Value}");
+            }
+            return true;
+        }
+
+        public static bool ReadDoubleFromXElementAttribute(XElement node, string attributeName, out double result,
+            bool required = true, double defaultValue = 0.0f)
+        {
+            if (!GetXAttribute(node, attributeName, out XAttribute xAttribute, required))
+            {
                 result = defaultValue;
                 return false;
+            }
+
+            bool parseSuccess = double.TryParse(xAttribute.Value, out result);
+            if (!parseSuccess)
+            {
+                throw new Exception($"Attribute {node.Name}/{xAttribute.Name.LocalName} expected a double, received: {xAttribute.Value}");
             }
             return true;
         }
@@ -118,6 +154,14 @@ namespace Unity.Robotics.UrdfImporter
             bool required = true, float defaultValue = 0.0f)
         {
             bool wasSuccess = ReadDoubleFromXElement(node, childElementName, out double resultAsDouble, required, defaultValue);
+            result = (float) resultAsDouble;
+            return wasSuccess;
+        }
+
+        public static bool ReadFloatFromXElementAttribute(XElement node, string attributeName, out float result,
+            bool required = true, float defaultValue = 0.0f)
+        {
+            bool wasSuccess = ReadDoubleFromXElementAttribute(node, attributeName, out double resultAsDouble, required, defaultValue);
             result = (float) resultAsDouble;
             return wasSuccess;
         }
