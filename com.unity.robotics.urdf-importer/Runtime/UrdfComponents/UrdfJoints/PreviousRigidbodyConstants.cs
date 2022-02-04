bb925a58f3146cd043a785884ddf29c41056a962
p3te <p96.smith@qut.edu.au>
2022-02-04 17:14:01 +1000

    Working on improving buoyancy stabilisation, especially for long thin objects.

--- com.unity.robotics.urdf-importer/Runtime/UrdfComponents/UrdfJoints/PreviousRigidbodyConstants.cs
+++ com.unity.robotics.urdf-importer/Runtime/UrdfComponents/UrdfJoints/PreviousRigidbodyConstants.cs
@@ -10,5 +10,29 @@ namespace Unity.Robotics.UrdfImporter
         [SerializeField] public float angularDrag;
         [SerializeField] public Vector3 centerOfMass;
 
+        public void SetValues(UrdfLinkDescription link)
+        {
+            this.mass = (float) link.inertial.mass;
+            //TODO - Find the drag & angularDrag...
+            //previousRigidbodyConstants.drag = link.inertial.;
+            //previousRigidbodyConstants.angularDrag = rigidbody.angularDrag;
+            if (link.inertial.origin == null)
+            {
+                RuntimeUrdf.AddImportWarning($"Missing link.inertial.origin for link with name {link.name}, assuming {this.centerOfMass}");
+            }
+            else
+            {
+                this.centerOfMass = link.inertial.origin.Xyz.ToVector3().Ros2Unity();
+            }
+        }
+        
+        public void SetValues(Rigidbody from)
+        {
+            this.mass = from.mass;
+            this.drag = from.drag;
+            this.angularDrag = from.angularDrag;
+            this.centerOfMass = from.centerOfMass;
+        }
+
     }
 }
